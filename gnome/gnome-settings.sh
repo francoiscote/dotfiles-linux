374beb5d105217e73107f708be40ba095e28f797
François Côté <francois@francoiscote.net>
2018-01-11 09:10:51 -0500

    gsettings keyboard

---
+++ gnome/gnome-settings.sh
@@ -0,0 +1,4 @@
+# Keyboard rate
+gsettings set org.gnome.desktop.peripherals.keyboard repeat true
+gsettings set org.gnome.desktop.peripherals.keyboard delay 200
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 15
