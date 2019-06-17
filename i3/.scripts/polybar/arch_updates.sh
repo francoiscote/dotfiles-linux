5c4a0825bdadb5c4874e2c21a7c62436aa3a547c
François Côté <francois@francoiscote.net>
2019-06-17 07:19:52 -0400

    polybar: fix arch_updates script

--- i3/.scripts/polybar/arch_updates.sh
+++ i3/.scripts/polybar/arch_updates.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+source /usr/share/fonts/awesome-terminal-fonts/fontawesome-regular.sh
+
 pac=$(checkupdates | wc -l)
-# aur=$(cower -u --timeout=20 | wc -l)
-aur=0
+aur=$(yay -Qu --aur | wc -l)
 check=$((pac + aur))
 if [[ "$check" != "0" ]]
 then
-    echo "$pac %{F#5b5b5b}%{F-} $aur"
+    echo -e "$pac %{F#5b5b5b}\u$CODEPOINT_OF_AWESOME_CLOUD_DOWNLOAD%{F-} $aur"
 fi
